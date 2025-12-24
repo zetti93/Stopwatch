a9ffafc57ced53cc540ad2d7032f858220d7405d
zetti93 <geraldo.donizetti93@hotmail.com>
2025-12-24 17:45:01 -0300

    Criado o menu, e a função de calculo de tempo.

--- Stopwatch/Program.cs
+++ Stopwatch/Program.cs
@@ -7,12 +7,48 @@ namespace Stopwatch
     {
         static void Main(string[] args)
         {
-            Start();
+            //Start(6);
+            Menu();
         }
 
-        static void Start()
+        static void Menu()
         {
-            var time = 10;
+            Console.Clear();
+            Console.WriteLine("S = Segundo - Exemplo 10s => 10 segundos");
+            Console.WriteLine("M = Minuto - Exemplo 1m => 1 Minuto");
+            Console.WriteLine("0 = Sair");
+            Console.WriteLine("");
+            Console.WriteLine("Quanto tempo deseja contar?");
+
+            string date = Console.ReadLine().ToLower();
+            char type = char.Parse(date.Substring(date.Length - 1, 1));
+            int time = int.Parse(date.Substring(0, date.Length - 1));
+            int multiplier = 1;
+
+            if (type == 'm')
+                multiplier = 60;
+
+            if (time == 0)
+                System.Environment.Exit(0);
+
+            PreStart(time * multiplier);
+        }
+
+        static void PreStart(int time)
+        {
+            Console.Clear();
+            Console.WriteLine("Ready...");
+            Thread.Sleep(1000);
+            Console.WriteLine("Set...");
+            Thread.Sleep(1000);
+            Console.WriteLine("Go...");
+            Thread.Sleep(2000);
+
+            Start(time);
+        }
+
+        static void Start(int time)
+        {
             var currentTime = 0;
 
             while (currentTime != time)
@@ -25,6 +61,11 @@ namespace Stopwatch
 
                 Thread.Sleep(1000);
             }
+
+            Console.Clear();
+            Console.WriteLine("Stopwatch finalizado");
+            Thread.Sleep(2500);
+            Menu();
         }
     }
 }
